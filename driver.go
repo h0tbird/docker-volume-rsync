3171d482b990497977fc8c7fc3b77c29b5edf589
Marc Villacorta <marc.villacorta@gmail.com>
2015-08-21 13:40:15 +0200

    Added documentation comments

--- driver.go
+++ driver.go
@@ -38,6 +38,15 @@ func (d myDummyDriver) Create(r dkvolume.Request) dkvolume.Response {
 
 //-----------------------------------------------------------------------------
 // POST /VolumeDriver.Remove
+//
+// Request:
+//  { "Name": "volume_name" }
+//  Delete the specified volume from disk. This request is issued when a user
+//  invokes docker rm -v to remove volumes associated with a container.
+//
+// Response:
+//  { "Err": null }
+//  Respond with a string error if an error occurred.
 //-----------------------------------------------------------------------------
 
 func (d myDummyDriver) Remove(r dkvolume.Request) dkvolume.Response {
@@ -46,6 +55,15 @@ func (d myDummyDriver) Remove(r dkvolume.Request) dkvolume.Response {
 
 //-----------------------------------------------------------------------------
 // POST /VolumeDriver.Path
+//
+// Request:
+//  { "Name": "volume_name" }
+//  Docker needs reminding of the path to the volume on the host.
+//
+// Response:
+//  { "Mountpoint": "/path/to/directory/on/host", "Err": null }
+//  Respond with the path on the host filesystem where the volume has been
+//  made available, and/or a string error if an error occurred.
 //-----------------------------------------------------------------------------
 
 func (d myDummyDriver) Path(r dkvolume.Request) dkvolume.Response {
@@ -54,6 +72,16 @@ func (d myDummyDriver) Path(r dkvolume.Request) dkvolume.Response {
 
 //-----------------------------------------------------------------------------
 // POST /VolumeDriver.Mount
+//
+// Request:
+//  { "Name": "volume_name" }
+//  Docker requires the plugin to provide a volume, given a user specified
+//  volume name. This is called once per container start.
+//
+// Response:
+//  { "Mountpoint": "/path/to/directory/on/host", "Err": null }
+//  Respond with the path on the host filesystem where the volume has been
+//  made available, and/or a string error if an error occurred.
 //-----------------------------------------------------------------------------
 
 func (d myDummyDriver) Mount(r dkvolume.Request) dkvolume.Response {
@@ -62,6 +90,16 @@ func (d myDummyDriver) Mount(r dkvolume.Request) dkvolume.Response {
 
 //-----------------------------------------------------------------------------
 // POST /VolumeDriver.Unmount
+//
+// Request:
+//  { "Name": "volume_name" }
+//  Indication that Docker no longer is using the named volume. This is called
+//  once per container stop. Plugin may deduce that it is safe to deprovision
+//  it at this point.
+//
+// Response:
+//  { "Err": null }
+//  Respond with a string error if an error occurred.
 //-----------------------------------------------------------------------------
 
 func (d myDummyDriver) Unmount(r dkvolume.Request) dkvolume.Response {
